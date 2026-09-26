8a38acf02e2cb9ab62f98675a8f401443db38402
Wang <2842wang6843w@gmail.com>
2026-09-26 13:41:29 +0800

    Sat Sep 26 13:41:29     2026

--- csrc/MID_plt/src/config/pl_cmd.h
+++ csrc/MID_plt/src/config/pl_cmd.h
@@ -1,16 +1,21 @@
 /*
-@@ 单图模式: 
->> arm 向 MODE_ADDR 写入 SINGLE_MODE
+选择模式:
+# (单图模式) arm 向 MODE_ADDR 写入 SINGLE_MODE
 
-载入图片:
->> arm 把新图片放到 SINGLE_IMG_ADDR
+    载入图片:
+    >> arm 把新图片放到 SINGLE_IMG_ADDR
 
-写入阈值:
->> arm 把新data值写到 DATA_REG_ADDR, pl 作为slave 自动相应, 更新 
+    写入阈值:
+    >> arm 把新data值写到 DATA_REG_ADDR, pl 作为slave 自动相应, 更新 
 
-开始分析:
->> arm 向 CMD_REG_ADDR 写入 REOPERATE
->>> pl 从 SINGLE_IMG_ADDR 读取图片再 operate 一遍, 然后清空 控制寄存器
+    开始分析:
+    >> arm 向 CMD_REG_ADDR 写入 REOPERATE
+    >>> pl 从 SINGLE_IMG_ADDR 读取图片再 operate 一遍, 然后清空 控制寄存器
+
+
+上位机操作大致就是:
+页面1: 先点"选择模式", 选择"单图模式", 然后进入单图模式页面
+页面2(单图模式页面): 先点"载入图片", 选择本地路径(.bin文件).  再点"写入阈值"(后续再加其他数据写入).  最后点"开始分析"
 
 */
 
