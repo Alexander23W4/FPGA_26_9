0a2d476dd1e6834935aa53ca2333c4591433ec77
Wang <2842wang6843w@gmail.com>
2026-09-26 11:55:51 +0800

    Sat Sep 26 11:55:51     2026

--- csrc/MID_plt/src/config/pl_cmd.h
+++ csrc/MID_plt/src/config/pl_cmd.h
@@ -1,12 +1,36 @@
 /*
-单图模式: 有新图片   重新操作    
+@@ 单图模式: 
+>> arm 向 MODE_ADDR 写入 SINGLE_MODE
+
+有新图片:
+>> arm 把新图片放到 SINGLE_IMG_ADDR
+
+重新操作:
+>> arm 向 CMD_REG_ADDR 写入 REOPERATE
+>>> pl 从 SINGLE_IMG_ADDR 读取图片再 operate 一遍, 然后清空 控制寄存器
+
+写入新阈值:
+>> arm 把新data值写到 DATA_REG_ADDR, pl 作为slave 自动相应, 更新 
 
 单图模式需要的状态寄存器: 
 
 
 */
 
+#define SINGLE_IMG_ADDR 0x10000000
+#define SINGLE_IMG_LEN  0x20000
+
+// 这两个是伪地址, pl读到这个地址就知道是 写 模式寄存器/控制寄存器/数据寄存器
+#define MODE_ADDR     0x01
+#define CMD_REG_ADDR  0x10
+#define DATA_REG_ADDR 0x20
+
+// 模式指令 (单图模式/视频流模式)
+#define SINGLE_MODE 0x01
+#define STREAM_MODE 0x02
+
+// 控制指令
+#define REOPERATE 0x01
 
 
-#define STREAM_MODE 1
 
